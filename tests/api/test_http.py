5c28bde7610ef30c658072f281151d0215a3ce1b
Anastasiia <stasiakoj@gmail.com>
2024-01-17 21:32:31 +0200

    Update http tests

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -6,7 +6,9 @@ def test_first_request():
     r = requests.get('https://api.github.com/zen')
     print(f"Response is {r.text}")
 
+@pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
-    print(f"Response is {r.text}")
-
+    print(f"Response Body is {r.json()}")
+    print(f"Response Status code is {r.status_code}")
+    print(f"Response Headers are {r.headers}")
