d477f81a9d7a0f6839f87ca9f63743afa870b1cc
Anastasiia <stasiakoj@gmail.com>
2024-01-17 21:41:22 +0200

    Add assertions into http tests

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -9,6 +9,15 @@ def test_first_request():
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
-    print(f"Response Body is {r.json()}")
-    print(f"Response Status code is {r.status_code}")
-    print(f"Response Headers are {r.headers}")
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+
+    assert r.status_code == 404
